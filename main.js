ef870e2a2ffcafd55cacb0c531dfa45d98497203
Kayden Clark <KaydenClark725@gmail.com>
2019-10-25 15:56:26 -0600

    added more objects

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.designMode = 'off'
 
 var clicks = 0;
 console.log("check 1")
+
 const getId = (input) => {
     let abrev = document.getElementById(input)
     return abrev
@@ -12,8 +13,17 @@ const b = getId("trip")
 const c = getId("quad")
 const d = getId("getfast")
 const e = getId("color")
+const g = getId("god")
 const propic = getId("img");
-let scores = [a, b, c, d, e, propic]
+const ex1 = getId("ex1")
+const ex2 = getId("ex2")
+const ex3 = getId("ex3")
+const ex4 = getId("ex4")
+const ex5 = getId("ex5")
+let bgColor = document.body.style.backgroundColor
+let scores = [a, b, c, d, e, g, ex1, ex2, ex3, ex4, ex5, propic]
+let ex = [ex1, ex2, ex3, ex4, ex5]
+
 
 
 console.log(propic)
@@ -22,28 +32,17 @@ const colorrandint = () => {
     let colors = 
     [`#FF9D00`, `#CF32FF`, `#00FFF2`, `#FF0010`, `#37FF00`, `#1BE7FF`, `#F9D800`, `#6EEB83`, `#FF5714`, `#FF82A9`, `#DE369D`, `#3185FC`, `#E84855`, `#0D21A1`, `#EA3788`, `#F26419`, `#FFFC31`, `#3EC300`, `#337CA0`, `#FF1D15`, `#E3170A`, `#0CCA4A`, `#0C8346`, `#7768AE`, `#9D75CB`]
     let randcolor = colors[(Math.floor(Math.random() * (colors.length)))]
-    document.body.style.backgroundColor = randcolor
+    bgColor = randcolor
 };
 
 console.log("check 2")
 
-const moveobject = () => {
-    propic.style.display = "block"
-    let x = Math.floor(Math.random() * 75)
-    let y = Math.floor(Math.random() * 75)
-    propic.style.margin = `${y}vh ${x}vw`;
+const moveobject = (input, number) => {
+    input.style.display = "block"
+    let x = Math.floor(Math.random() * number)
+    let y = Math.floor(Math.random() * number)
+    input.style.margin = `${y}vh ${x}vw`;
 };
-
-const copy = (moveobject) => {
-    let thecopy = {}
-    let key;
-    for (key in moveobject) {
-        thecopy[key] = moveobject[key];
-    }
-    return thecopy
-    console.log(thecopy)
-};
-
 
 const press1 = () => {
     clicks += 1;
@@ -67,17 +66,22 @@ const press1 = () => {
         d.style.display ="block";
         clicks++
     }
-    if (clicks >= 300) {
+    if (clicks >= 1) {
         d.style.display ="none"
         e.style.display ="block"
-        colorrandint()
-        moveobject()
-        copy()
+        if (clicks <= 5) {
+            colorrandint()
+            moveobject(propic, 75)
+        }
     }
-    if(clicks >= 500){}
-    
-    
-};
+    if(clicks >= 5){
+        e.style.display= "none"
+        propic.style.display= "none"
+        g.style.display= "block"
+        let i = Math.floor(Math.random() * ex.length)
+        moveobject(ex[i], 80)
+    }
+}
 
 let reset= () => {
     clicks = 0;
